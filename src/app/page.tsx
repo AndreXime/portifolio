015845fc2dffacf3df9da0a1804acbd354db401a
AndreXime <andreximenesa20@gmail.com>
2025-03-29 22:16:31 -0300

    PATCH: Efeito de fundo melhor e sessão sobre mim

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,57 +3,55 @@ import BackgroundEffect from '@/components/ui/BackgroundEffect';
 
 export default function Page() {
 	return (
-		<div className="min-h-screen ">
-			<main>
-				<section
-					id="about"
-					className="py-20 md:py-24 lg:py-32 px-5 min-h-[60vh] relative">
-					<BackgroundEffect />
-					<Hero />
-				</section>
+		<main className="min-h-screen">
+			<section
+				id="about"
+				className="py-20 md:py-24 lg:py-32 px-5 min-h-[70vh] relative">
+				<BackgroundEffect />
+				<Hero />
+			</section>
 
-				<section
-					id="projects"
-					className="py-12 md:py-24 lg:py-32 min-h-[60dvh] bg-green-50">
-					<div className="flex flex-col items-center justify-center space-y-4 text-center mb-10 px-4">
-						<div className="space-y-2">
-							<h2 className="heading text-3xl tracking-tighter sm:text-4xl md:text-5xl text-center">
-								Projetos principais
-							</h2>
-							<p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed ">
-								Em cada projeto, me esforcei ao máximo para aplicar todo o meu conhecimento, desde o backend para
-								desenvolver a API, passando pela infraestrutura para o deploy, até o design para criar a interface.
-							</p>
-						</div>
+			<section
+				id="projects"
+				className="py-12 md:py-24 lg:py-32 min-h-[60dvh] bg-green-50">
+				<div className="flex flex-col items-center justify-center space-y-4 text-center mb-10 px-4">
+					<div className="space-y-2">
+						<h2 className="heading text-3xl tracking-tighter sm:text-4xl md:text-5xl text-center">
+							Projetos principais
+						</h2>
+						<p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed ">
+							Em cada projeto, me esforcei ao máximo para aplicar todo o meu conhecimento, desde o backend para
+							desenvolver a API, passando pela infraestrutura para o deploy, até o design para criar a interface.
+						</p>
 					</div>
-					<Projetos />
-				</section>
+				</div>
+				<Projetos />
+			</section>
 
-				<section
-					id="tech"
-					className="py-12 md:py-24 lg:py-32 px-2 min-h-[60dvh]">
-					<div className="flex flex-col items-center justify-center space-y-4 text-center mb-10 px-4">
-						<div className="space-y-2">
-							<h2 className="heading text-3xl tracking-tighter sm:text-4xl md:text-5xl text-center">
-								Habilidade técnicas
-							</h2>
-							<p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed ">
-								Minhas habilidades foram adquiridas tanto na faculdade quanto por meio online, como YouTube e cursos.
-							</p>
-						</div>
+			<section
+				id="tech"
+				className="py-12 md:py-24 lg:py-32 px-2 min-h-[60dvh]">
+				<div className="flex flex-col items-center justify-center space-y-4 text-center mb-10 px-4">
+					<div className="space-y-2">
+						<h2 className="heading text-3xl tracking-tighter sm:text-4xl md:text-5xl text-center">
+							Habilidade técnicas
+						</h2>
+						<p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed ">
+							Minhas habilidades foram adquiridas tanto na faculdade quanto por meio online, como YouTube e cursos.
+						</p>
 					</div>
-					<TechStack />
-				</section>
+				</div>
+				<TechStack />
+			</section>
 
-				<section
-					id="contact"
-					className="py-12 md:py-24 lg:py-32 px-5 min-h-[40dvh] bg-green-50">
-					<div className="text-center items-center flex flex-col">
-						<h2 className="heading text-3xl tracking-tighter sm:text-4xl md:text-5xl mb-6">Fale comigo!</h2>
-						<Contact />
-					</div>
-				</section>
-			</main>
-		</div>
+			<section
+				id="contact"
+				className="py-12 md:py-24 lg:py-32 px-5 min-h-[40dvh] bg-green-50">
+				<div className="text-center items-center flex flex-col">
+					<h2 className="heading text-3xl tracking-tighter sm:text-4xl md:text-5xl mb-6">Fale comigo!</h2>
+					<Contact />
+				</div>
+			</section>
+		</main>
 	);
 }
--- src/components/ui/BackgroundEffect.tsx
+++ src/components/ui/BackgroundEffect.tsx
@@ -2,48 +2,51 @@
  * O container pai deve ter propriedade relativa
  */
 export default function BackgroundEffect() {
-	const polygon =
-		'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)';
+	const polygon = 'polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)';
 	const polygonClass = 'absolute bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]';
 
 	return (
-		<div className="absolute inset-0 -z-10 overflow-hidden blur-xl ">
-			{/* Forma 1 - Canto superior esquerdo */}
+		<div className="absolute inset-0 -z-10 overflow-hidden blur-xl md:blur-2xl">
+			{/* Canto superior esquerdo */}
 			<div
 				style={{ clipPath: polygon }}
-				className={'left-[-10%] top-[-10%] w-[40%] h-[40%] rotate-[15deg] opacity-30 float-animation ' + polygonClass}
+				className={`left-[-10%] top-[-10%] w-[40%] h-[40%] rotate-[15deg] opacity-30 float-animation ${polygonClass}`}
 			/>
 
-			{/* Forma 2 - Canto inferior direito */}
+			{/* Canto superior direito */}
 			<div
 				style={{ clipPath: polygon }}
-				className={
-					'right-[-15%] bottom-[-10%] w-[50%] h-[50%] rotate-[45deg] opacity-40 float-animation ' + polygonClass
-				}
+				className={`right-[-10%] top-[-10%] w-[40%] h-[40%] rotate-[20deg] opacity-30 float-animation ${polygonClass}`}
 			/>
 
-			{/* Forma 3 - Centro-direita */}
+			{/* Canto inferior esquerdo */}
 			<div
 				style={{ clipPath: polygon }}
-				className={'right-[10%] top-[30%] w-[35%] h-[35%] rotate-[25deg] opacity-25 float-animation' + polygonClass}
+				className={`left-[-10%] bottom-[-10%] w-[50%] h-[50%] rotate-[-20deg] opacity-40 float-animation ${polygonClass}`}
 			/>
 
-			{/* Forma 4 - Centro-esquerda */}
+			{/* Canto inferior direito */}
 			<div
 				style={{ clipPath: polygon }}
-				className={'left-[5%] bottom-[30%] w-[45%] h-[45%] rotate-[10deg] opacity-35 float-animation ' + polygonClass}
+				className={`right-[-15%] bottom-[-10%] w-[50%] h-[50%] rotate-[45deg] opacity-40 float-animation ${polygonClass}`}
 			/>
 
-			{/* Polígono - Canto superior direito */}
+			{/* Meio-esquerda */}
 			<div
 				style={{ clipPath: polygon }}
-				className={`${polygonClass} right-[-10%] top-[-10%] w-[40%] h-[40%] rotate-[20deg] opacity-30 float-animation`}
+				className={`left-[10%] top-[50%] w-[35%] h-[35%] rotate-[25deg] opacity-25 float-animation ${polygonClass}`}
 			/>
 
-			{/* Polígono - Canto inferior esquerdo */}
+			{/* Meio-direita */}
 			<div
 				style={{ clipPath: polygon }}
-				className={`${polygonClass} left-[-10%] bottom-[-10%] w-[50%] h-[50%] rotate-[-20deg] opacity-40 float-animation`}
+				className={`right-[10%] top-[50%] w-[35%] h-[35%] rotate-[-25deg] opacity-25 float-animation ${polygonClass}`}
+			/>
+
+			{/* Centro superior */}
+			<div
+				style={{ clipPath: polygon }}
+				className={`left-[50%] top-[10%] w-[30%] h-[30%] -translate-x-1/2 rotate-[180deg] opacity-35 float-animation ${polygonClass}`}
 			/>
 		</div>
 	);
