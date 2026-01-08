4ff693fe33e5e19e46f2568831aac8748fb702d1
Andre Ximenes <andreximenes536@gmail.com>
2026-01-08 14:35:48 -0300

    Astro usando adapter para cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,26 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
-import vercel from "@astrojs/vercel";
 
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  site: "https://andreximenes.xyz",
-  integrations: [
-    preact(),
-    sitemap({
-      filter: (page) => page !== "https://andreximenes.xyz/404",
-    }),
-  ],
-  build: {
-    inlineStylesheets: "always",
-  },
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  adapter: vercel(),
+	output: "static",
+	site: "https://andreximenes.xyz",
+	integrations: [
+		preact(),
+		sitemap({
+			filter: (page) => page !== "https://andreximenes.xyz/404",
+		}),
+	],
+	build: {
+		inlineStylesheets: "always",
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	adapter: cloudflare(),
 });
