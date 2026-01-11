0994a362d79effd85260f8615e2ab16e571156b7
Andre Ximenes <andreximenes536@gmail.com>
2026-01-11 10:31:39 -0300

    Projeto usando biome

--- src/content/tecnologies.ts
+++ src/content/tecnologies.ts
@@ -1,32 +1,32 @@
 import {
-  TypescriptIcon,
-  NextjsIcon,
-  TailwindIcon,
-  GitIcon,
-  NodeJSIcon,
-  BunIcon,
-  HonoIcon,
-  PostgreeICon,
-  DockerIcon,
-  LinuxIcon,
-  CICDIcon,
-  AWSIcon,
-  GoIcon,
-  GithubIcon,
-  AstroIcon,
-  GinIcon,
-  RedisIcon,
+	TypescriptIcon,
+	NextjsIcon,
+	TailwindIcon,
+	GitIcon,
+	NodeJSIcon,
+	BunIcon,
+	HonoIcon,
+	PostgreeICon,
+	DockerIcon,
+	LinuxIcon,
+	CICDIcon,
+	AWSIcon,
+	GoIcon,
+	GithubIcon,
+	AstroIcon,
+	GinIcon,
+	RedisIcon,
 } from "@/components/Icons";
 
 interface TechItem {
-  name: string;
-  role: string;
-  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
-  secondIcon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+	name: string;
+	role: string;
+	icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+	secondIcon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
 }
 
 export const words = [
-  /*
+	/*
   "CORS",
   "API Restful",
   "Engenharia de software",
@@ -36,45 +36,45 @@ export const words = [
 ];
 
 export const technologies: TechItem[] = [
-  { name: "TypeScript", role: "Linguagem Principal", icon: TypescriptIcon },
-  {
-    name: "Astro & Next.js",
-    role: "Frontend Framework",
-    icon: AstroIcon,
-    secondIcon: NextjsIcon,
-  },
-  { name: "Tailwind CSS & ShadCN", role: "Estilização", icon: TailwindIcon },
-  {
-    name: "Node.js & Bun",
-    role: "Javascript Runtime",
-    icon: NodeJSIcon,
-    secondIcon: BunIcon,
-  },
-  {
-    name: "Git & GitHub",
-    role: "Versionamento",
-    icon: GitIcon,
-    secondIcon: GithubIcon,
-  },
-  {
-    name: "Hono & Gin",
-    role: "API Framework",
-    icon: HonoIcon,
-    secondIcon: GinIcon,
-  },
-  {
-    name: "PostgreSQL & Redis",
-    role: "Banco de Dados",
-    icon: PostgreeICon,
-    secondIcon: RedisIcon,
-  },
-  { name: "Docker", role: "Orquestrador de containers", icon: DockerIcon },
-  { name: "Linux", role: "Sistema operacional", icon: LinuxIcon },
-  {
-    name: "Github Actions",
-    role: "Automação de Workflow (CI/CD)",
-    icon: CICDIcon,
-  },
-  { name: "AWS & Vercel", role: "Hospedagem", icon: AWSIcon },
-  { name: "Golang", role: "Linguagem secundária", icon: GoIcon },
+	{ name: "TypeScript", role: "Linguagem Principal", icon: TypescriptIcon },
+	{
+		name: "Astro e Next.js",
+		role: "Frontend Framework",
+		icon: AstroIcon,
+		secondIcon: NextjsIcon,
+	},
+	{ name: "Tailwind CSS e ShadCN", role: "Estilização", icon: TailwindIcon },
+	{
+		name: "Node.js & Bun",
+		role: "Javascript Runtime",
+		icon: NodeJSIcon,
+		secondIcon: BunIcon,
+	},
+	{
+		name: "Git e GitHub",
+		role: "Versionamento",
+		icon: GitIcon,
+		secondIcon: GithubIcon,
+	},
+	{
+		name: "Hono & Gin",
+		role: "API Framework",
+		icon: HonoIcon,
+		secondIcon: GinIcon,
+	},
+	{
+		name: "PostgreSQL & Redis",
+		role: "Banco de Dados",
+		icon: PostgreeICon,
+		secondIcon: RedisIcon,
+	},
+	{ name: "Docker", role: "Orquestrador de containers", icon: DockerIcon },
+	{ name: "Linux", role: "Sistema operacional", icon: LinuxIcon },
+	{
+		name: "Github Actions",
+		role: "Automação de Workflow (CI/CD)",
+		icon: CICDIcon,
+	},
+	{ name: "AWS & Vercel", role: "Hospedagem", icon: AWSIcon },
+	{ name: "Golang", role: "Linguagem secundária", icon: GoIcon },
 ];
