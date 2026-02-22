9c953bafc8cfa58c01b66c962e72d93a5f905941
Andre Ximenes <andreximenes536@gmail.com>
2026-02-21 23:59:34 -0300

    fix(content): remover barra final da URL do LinkedIn

--- src/content/social.ts
+++ src/content/social.ts
@@ -1,5 +1,5 @@
 export const socialLinks = {
 	email: "andreximenesa20@gmail.com",
 	github: "https://github.com/AndreXime",
-	linkedin: "https://www.linkedin.com/in/andreximenesdev/",
+	linkedin: "https://www.linkedin.com/in/andreximenesdev",
 };
